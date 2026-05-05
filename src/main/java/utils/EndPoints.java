3d5189d94e3f39f9fd8d36191dac41c6f8ff6bce
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-05-05 17:19:24 -0300

    feat: estruturar testes de usuário com setup, configurações iniciais e primeiros testes

--- src/main/java/utils/EndPoints.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class EndPoints {
-
-    protected static final String BASE_URI = "http://localhost:8080/";
-    protected static final String USUARIOS = "usuarios/";
-
-}
---
+++ src/test/java/utlis/EndPoints.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class EndPoints {
+
+    public static final String BASE_URI = "http://localhost:8080/";
+    public static final String USUARIOS = "usuarios";
+    public static final String USUARIOS_ID = "usuarios/{id}";
+
+}
