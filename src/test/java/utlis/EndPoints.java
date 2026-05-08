8caa3bb7b9ad134d9fd5be1b0eefba71e5a11e89
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-05-08 17:16:03 -0300

    feat: configurar endpoint LOGINS na classe EndPoints

--- src/test/java/utlis/EndPoints.java
+++ src/test/java/utlis/EndPoints.java
@@ -7,5 +7,6 @@ public class EndPoints {
     public static final String USUARIOS_ID = "usuarios/{id}";
     public static final String USUARIOS_ID_DADOS_ADICIONAIS = "usuarios/{id}/dados-adicionais";
     public static final String USUARIOS_ID_ALTERAR_SENHA = "usuarios/{id}/alterar-senha";
+    public static final String LOGINS = "logins";
 
 }
