bee5ffe833e0c083898d67c60ef54ab7bdec5e7a
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-05-08 17:19:18 -0300

    feat(login): criar LoginFactory para geração de dados de login

---
+++ src/test/java/factories/LoginFactory.java
@@ -0,0 +1,65 @@
+package factories;
+
+import clients.UsuarioClient;
+import models.LoginModel;
+import models.UsuarioModel;
+import utils.DataFakerUtils;
+import utlis.ConfigProperties;
+
+public class LoginFactory {
+
+    public static LoginModel usuarioLeitorSucesso() {
+        UsuarioModel usuario = UsuarioFactory.criarUsuario();
+        UsuarioClient.criarUsuario(usuario);
+
+        LoginModel usuarioLogin = new LoginModel();
+        usuarioLogin.setUser(usuario.getEmail());
+        usuarioLogin.setSenha(usuario.getSenha());
+        return usuarioLogin;
+    }
+
+    public static LoginModel usuarioLeitorSenhaInvalida() {
+        UsuarioModel usuario = UsuarioFactory.criarUsuario();
+        UsuarioClient.criarUsuario(usuario);
+
+        LoginModel usuarioLogin = new LoginModel();
+        usuarioLogin.setUser(usuario.getEmail());
+        usuarioLogin.setSenha(DataFakerUtils.senhaAleatoria());
+        return usuarioLogin;
+    }
+
+    public static LoginModel usuarioLeitorEmailInexistente() {
+        LoginModel usuarioLogin = new LoginModel();
+        usuarioLogin.setUser(DataFakerUtils.email());
+        usuarioLogin.setSenha(DataFakerUtils.senhaAleatoria());
+        return usuarioLogin;
+    }
+
+    public static LoginModel usuarioAdministradorEmailComSucesso() {
+        LoginModel usuarioLogin = new LoginModel();
+        usuarioLogin.setUser(ConfigProperties.get("admin.email"));
+        usuarioLogin.setSenha(ConfigProperties.get("admin.password"));
+        return usuarioLogin;
+    }
+
+    public static LoginModel usuarioAdministradorSenhaInvalida() {
+        LoginModel usuarioLogin = new LoginModel();
+        usuarioLogin.setUser(ConfigProperties.get("admin.email"));
+        usuarioLogin.setSenha(DataFakerUtils.senhaAleatoria());
+        return usuarioLogin;
+    }
+
+    public static LoginModel usuarioModeradorEmailComSucesso() {
+        LoginModel usuarioLogin = new LoginModel();
+        usuarioLogin.setUser(ConfigProperties.get("moderador.email"));
+        usuarioLogin.setSenha(ConfigProperties.get("moderador.password"));
+        return usuarioLogin;
+    }
+
+    public static LoginModel usuarioModeradorSenhaInvalida() {
+        LoginModel usuarioLogin = new LoginModel();
+        usuarioLogin.setUser(ConfigProperties.get("moderador.email"));
+        usuarioLogin.setSenha(DataFakerUtils.senhaAleatoria());
+        return usuarioLogin;
+    }
+}
