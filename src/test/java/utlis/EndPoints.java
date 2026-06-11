0e61679b7ec77e61877d23ce52348037e4bf86ce
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-06-11 10:56:33 -0300

    feat(livros): adicionar endpoints de livro, busca por ISBN e capa

--- src/test/java/utlis/EndPoints.java
+++ src/test/java/utlis/EndPoints.java
@@ -7,5 +7,8 @@ public class EndPoints {
     public static final String USUARIOS_DADOS_ADICIONAIS = "usuarios/dados-adicionais";
     public static final String USUARIOS_ALTERAR_SENHA = "usuarios/alterar-senha";
     public static final String LOGINS = "logins";
+    public static final String LIVROS = "livros";
+    public static final String LIVROS_ISBN = "livros/buscar/{isbn}";
+    public static final String LIVROS_ID_CAPA = "livros/{id}/capa";
 
 }
