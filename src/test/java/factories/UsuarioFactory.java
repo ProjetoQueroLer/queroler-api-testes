77694c2b986f7ca683d46466beee35e01ea5c215
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-06-18 11:26:49 -0300

    feat(factory): adicionar cenarios de usuario para testes de validacao

--- src/test/java/factories/UsuarioFactory.java
+++ src/test/java/factories/UsuarioFactory.java
@@ -2,7 +2,7 @@ package factories;
 
 import models.UsuarioModel;
 import utils.DataFakerUtils;
-import utlis.CarregarImagem;
+//import utlis.CarregarImagem;
 
 public class UsuarioFactory {
 
@@ -12,6 +12,72 @@ public class UsuarioFactory {
 
         usuario.setNome(DataFakerUtils.nome());
         String email = DataFakerUtils.email();
+        usuario.setEmail(email);
+        usuario.setConfirmarEmail(email);
+        usuario.setSenha("Teste@123");
+        usuario.setConfirmarSenha("Teste@123");
+        usuario.setCpf(DataFakerUtils.cpf());
+        usuario.setDataDeNascimento(DataFakerUtils.dataNascimento());
+        usuario.setCheckTermo(true);
+
+        return usuario;
+    }
+
+    public static UsuarioModel criarUsuarioNomeMais80Caracteres() {
+
+        UsuarioModel usuario = new UsuarioModel();
+
+        usuario.setNome(DataFakerUtils.nomeMais80Caracteres());
+        String email = DataFakerUtils.email();
+        usuario.setEmail(email);
+        usuario.setConfirmarEmail(email);
+        usuario.setSenha("Teste@123");
+        usuario.setConfirmarSenha("Teste@123");
+        usuario.setCpf(DataFakerUtils.cpf());
+        usuario.setDataDeNascimento(DataFakerUtils.dataNascimento());
+        usuario.setCheckTermo(true);
+
+        return usuario;
+    }
+
+    public static UsuarioModel criarUsuarioEmailMais256Caracteres(String numero) {
+
+        UsuarioModel usuario = new UsuarioModel();
+
+        usuario.setNome(DataFakerUtils.nome());
+        String email = DataFakerUtils.emailMais256Caracteres(numero);
+        usuario.setEmail(email);
+        usuario.setConfirmarEmail(email);
+        usuario.setSenha("Teste@123");
+        usuario.setConfirmarSenha("Teste@123");
+        usuario.setCpf(DataFakerUtils.cpf());
+        usuario.setDataDeNascimento(DataFakerUtils.dataNascimento());
+        usuario.setCheckTermo(true);
+
+        return usuario;
+    }
+
+    public static UsuarioModel criarUsuarioEmailDiferenteEmailConfirmar() {
+
+        UsuarioModel usuario = new UsuarioModel();
+
+        usuario.setNome(DataFakerUtils.nome());
+        usuario.setEmail(DataFakerUtils.email());
+        usuario.setConfirmarEmail(DataFakerUtils.email());
+        usuario.setSenha("Teste@123");
+        usuario.setConfirmarSenha("Teste@123");
+        usuario.setCpf(DataFakerUtils.cpf());
+        usuario.setDataDeNascimento(DataFakerUtils.dataNascimento());
+        usuario.setCheckTermo(true);
+
+        return usuario;
+    }
+
+    public static UsuarioModel criarUsuarioEmailInvalido(String email) {
+
+        UsuarioModel usuario = new UsuarioModel();
+
+        usuario.setNome(DataFakerUtils.nome());
         usuario.setEmail(email);
         usuario.setConfirmarEmail(email);
         usuario.setSenha("Teste@123");
