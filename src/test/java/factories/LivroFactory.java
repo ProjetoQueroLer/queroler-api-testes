1c0ac8e93d716db303cf576a58b5d2e41e64741d
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-06-23 11:38:35 -0300

    feat(livro): criar massa de teste para campos obrigatorios e valores invalidos

--- src/test/java/factories/LivroFactory.java
+++ src/test/java/factories/LivroFactory.java
@@ -76,4 +76,157 @@ public class LivroFactory {
         return livro;
     }
 
+    public static LivroModel criarLivroTituloVazio() {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo("");
+        livro.setIsbn(DataFakerUtils.isbn13());
+        livro.setEditora(DataFakerUtils.editora());
+        livro.setAnoDePublicacao(DataFakerUtils.ano());
+        livro.setNumeroDePaginas(DataFakerUtils.numeroDePagina());
+        livro.setIdioma(DataFakerUtils.idioma());
+        livro.setSinopse(DataFakerUtils.sinopse());
+        AutorModel autor = new AutorModel();
+        autor.setNome(DataFakerUtils.nome());
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
+    public static LivroModel criarLivroEditoraVazio() {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo(DataFakerUtils.titulo());
+        livro.setIsbn(DataFakerUtils.isbn13());
+        livro.setEditora("");
+        livro.setAnoDePublicacao(DataFakerUtils.ano());
+        livro.setNumeroDePaginas(DataFakerUtils.numeroDePagina());
+        livro.setIdioma(DataFakerUtils.idioma());
+        livro.setSinopse(DataFakerUtils.sinopse());
+        AutorModel autor = new AutorModel();
+        autor.setNome(DataFakerUtils.nome());
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
+    public static LivroModel criarLivroIsbnVazio() {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo(DataFakerUtils.titulo());
+        livro.setIsbn("");
+        livro.setEditora(DataFakerUtils.editora());
+        livro.setAnoDePublicacao(DataFakerUtils.ano());
+        livro.setNumeroDePaginas(DataFakerUtils.numeroDePagina());
+        livro.setIdioma(DataFakerUtils.idioma());
+        livro.setSinopse(DataFakerUtils.sinopse());
+        AutorModel autor = new AutorModel();
+        autor.setNome(DataFakerUtils.nome());
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
+    public static LivroModel criarLivroAnoPublicacaoVazio() {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo(DataFakerUtils.titulo());
+        livro.setIsbn(DataFakerUtils.isbn13());
+        livro.setEditora(DataFakerUtils.editora());
+        livro.setAnoDePublicacao("");
+        livro.setNumeroDePaginas(DataFakerUtils.numeroDePagina());
+        livro.setIdioma(DataFakerUtils.idioma());
+        livro.setSinopse(DataFakerUtils.sinopse());
+        AutorModel autor = new AutorModel();
+        autor.setNome(DataFakerUtils.nome());
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
+    public static LivroModel criarLivroPaginasVazio() {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo(DataFakerUtils.titulo());
+        livro.setIsbn(DataFakerUtils.isbn13());
+        livro.setEditora(DataFakerUtils.editora());
+        livro.setAnoDePublicacao(DataFakerUtils.ano());
+        livro.setNumeroDePaginas(null);
+        livro.setIdioma(DataFakerUtils.idioma());
+        livro.setSinopse(DataFakerUtils.sinopse());
+        AutorModel autor = new AutorModel();
+        autor.setNome(DataFakerUtils.nome());
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
+    public static LivroModel criarLivroPaginasNumeroNegativoEZero(Integer numero) {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo(DataFakerUtils.titulo());
+        livro.setIsbn(DataFakerUtils.isbn13());
+        livro.setEditora(DataFakerUtils.editora());
+        livro.setAnoDePublicacao(DataFakerUtils.ano());
+        livro.setNumeroDePaginas(numero);
+        livro.setIdioma(DataFakerUtils.idioma());
+        livro.setSinopse(DataFakerUtils.sinopse());
+        AutorModel autor = new AutorModel();
+        autor.setNome(DataFakerUtils.nome());
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
+    public static LivroModel criarLivroIdiomaVazio() {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo(DataFakerUtils.titulo());
+        livro.setIsbn(DataFakerUtils.isbn13());
+        livro.setEditora(DataFakerUtils.editora());
+        livro.setAnoDePublicacao(DataFakerUtils.ano());
+        livro.setNumeroDePaginas(DataFakerUtils.numeroDePagina());
+        livro.setIdioma("");
+        livro.setSinopse(DataFakerUtils.sinopse());
+        AutorModel autor = new AutorModel();
+        autor.setNome(DataFakerUtils.nome());
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
+    public static LivroModel criarLivroSinopseVazio() {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo(DataFakerUtils.titulo());
+        livro.setIsbn(DataFakerUtils.isbn13());
+        livro.setEditora(DataFakerUtils.editora());
+        livro.setAnoDePublicacao(DataFakerUtils.ano());
+        livro.setNumeroDePaginas(DataFakerUtils.numeroDePagina());
+        livro.setIdioma(DataFakerUtils.idioma());
+        livro.setSinopse("");
+        AutorModel autor = new AutorModel();
+        autor.setNome(DataFakerUtils.nome());
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
+    public static LivroModel criarLivroAutorVazio() {
+
+        LivroModel livro = new LivroModel();
+
+        livro.setTitulo(DataFakerUtils.titulo());
+        livro.setIsbn(DataFakerUtils.isbn13());
+        livro.setEditora(DataFakerUtils.editora());
+        livro.setAnoDePublicacao(DataFakerUtils.ano());
+        livro.setNumeroDePaginas(DataFakerUtils.numeroDePagina());
+        livro.setIdioma(DataFakerUtils.idioma());
+        livro.setSinopse(DataFakerUtils.sinopse());
+        AutorModel autor = new AutorModel();
+        autor.setNome("");
+        livro.setAutores(List.of(autor));
+        return livro;
+    }
+
 }
