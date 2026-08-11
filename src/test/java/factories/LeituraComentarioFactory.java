3e79bf8138dbea9c890ecbfb7e5cf5e3beeaf5b3
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-08-11 09:13:30 -0300

    refactor: adiciona metodos DataFaker para leitura

--- src/test/java/factories/LeituraComentarioFactory.java
+++ src/test/java/factories/LeituraComentarioFactory.java
@@ -1,15 +1,19 @@
 package factories;
 
 import models.LeituraComentarioModel;
+import utils.DataFakerUtils;
 
 public class LeituraComentarioFactory {
 
     public static LeituraComentarioModel criarLeituraComentario() {
         LeituraComentarioModel leituraComentarioModel = new LeituraComentarioModel();
 
-        leituraComentarioModel.setPaginaInicial(1);
-        leituraComentarioModel.setPaginaFinal(60);
-        leituraComentarioModel.setComentario("Comecei a leitura, otimo!");
+        int paginaInicial = DataFakerUtils.paginaInicial();
+        int paginaFinal = DataFakerUtils.paginaFinal(paginaInicial);
+
+        leituraComentarioModel.setPaginaInicial(paginaInicial);
+        leituraComentarioModel.setPaginaFinal(paginaFinal);
+        leituraComentarioModel.setComentario(DataFakerUtils.comentario());
 
         return leituraComentarioModel;
     }
