de0d8936783ccfd682696c6282f5d6421dd551c8
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-08-25 17:00:12 -0300

    test: adiciona metodos teste negativo

--- src/test/java/queroLerTests/metas/MetaCriarTest.java
+++ src/test/java/queroLerTests/metas/MetaCriarTest.java
@@ -2,14 +2,25 @@ package queroLerTests.metas;
 
 import baseTest.BaseTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import factories.LivroFactory;
 import factories.MetaFactory;
+import io.restassured.response.Response;
+import models.LivroModel;
 import models.MetaModel;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import queroLerTests.usuarios.UsuarioCadastrarTest;
 import report.Setup;
+import utils.DataFakerUtils;
+import utils.LivroHelper;
 import utils.MetaHelper;
 import utils.UsuarioHelper;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.equalTo;
 
 @ExtendWith(Setup.class)
 public class MetaCriarTest extends BaseTest {
@@ -51,6 +62,134 @@ public class MetaCriarTest extends BaseTest {
                 .log().body()
                 .statusCode(201);
 
+    }
+
+    @Test
+    public void adicionarMetaLeitorComLivro() throws IOException {
+        String token = UsuarioCadastrarTest.cadastrarUsuarioToken();
+
+        LivroModel livro = LivroFactory.criarLivroIsbn13();
+        Response responseLivro = LivroHelper.criarLivroCadastrar(token, livro);
+        responseLivro
+                .then()
+                .log().body()
+                .statusCode(201);
+
+        int livroId = responseLivro.jsonPath().getInt("id");
+
+        MetaModel metaLeitura = MetaFactory.adicionarMetaValida();
+
+        MetaHelper.adicionarMeta(token, metaLeitura)
+                .then()
+                .log().body()
+                .statusCode(201);
+
+        MetaHelper.adicionarLivroMeta(token, livroId)
+                .then()
+                .log().body()
+                .statusCode(204);
+
+    }
+
+    @Test
+    public void adicionarMetaAnoJaExistente() throws JsonProcessingException {
+        String token = UsuarioCadastrarTest.cadastrarUsuarioToken();
+
+        MetaModel metaLeitura = MetaFactory.adicionarMetaValida();
+        metaLeitura.setAno(DataFakerUtils.anoAtual());
+
+        MetaHelper.adicionarMeta(token, metaLeitura)
+                .then()
+                .log().body()
+                .statusCode(201);
+
+        MetaHelper.adicionarMeta(token, metaLeitura)
+                .then()
+                .log().body()
+                .statusCode(409)
+                .body(equalTo("Já há meta cadastrada para o ano de: " + metaLeitura.getAno()+"."));
+
+    }
+
+    @Test
+    public void adicionarMetaAnteriorAnoAtual() throws JsonProcessingException {
+        String token = UsuarioCadastrarTest.cadastrarUsuarioToken();
+
+        MetaModel metaLeitura = MetaFactory.adicionarMetaValida();
+        metaLeitura.setAno(DataFakerUtils.anoAtual()-1);
+
+        MetaHelper.adicionarMeta(token, metaLeitura)
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body(equalTo("O ano informado não pode ser anterior ao corrente."));
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {
+            20266,
+            123456789,
+            987654321
+    })
+    public void adicionarMetaAnoInvalido(int ano) throws JsonProcessingException {
+        String token = UsuarioCadastrarTest.cadastrarUsuarioToken();
+
+        MetaModel metaLeitura = MetaFactory.adicionarMetaValida();
+        metaLeitura.setAno(ano);
+
+        MetaHelper.adicionarMeta(token, metaLeitura)
+                .then()
+                .log().body()
+                .statusCode(400)
+//                .body(equalTo("O ano informado não pode ser anterior ao corrente."))
+        ;
+
+    }
+
+    @Test
+    public void adicionarMetaValorNegativoLivroAno() throws JsonProcessingException {
+        String token = UsuarioCadastrarTest.cadastrarUsuarioToken();
+
+        MetaModel metaLeitura = MetaFactory.adicionarMetaValida();
+        metaLeitura.setMetaLivrosAno(-1);
+
+        MetaHelper.adicionarMeta(token, metaLeitura)
+                .then()
+                .log().body()
+                .statusCode(400)
+//                .body(equalTo("O ano informado não pode ser anterior ao corrente."))
+        ;
+    }
+
+    @Test
+    public void adicionarMetaValorNegativoLivroMes() throws JsonProcessingException {
+        String token = UsuarioCadastrarTest.cadastrarUsuarioToken();
+
+        MetaModel metaLeitura = MetaFactory.adicionarMetaValida();
+        metaLeitura.setMetaLivrosMes(-1);
+
+        MetaHelper.adicionarMeta(token, metaLeitura)
+                .then()
+                .log().body()
+                .statusCode(400)
+//                .body(equalTo("O ano informado não pode ser anterior ao corrente."))
+        ;
+    }
+
+    @Test
+    public void adicionarMetaValorNegativoPaginasDia() throws JsonProcessingException {
+        String token = UsuarioCadastrarTest.cadastrarUsuarioToken();
+
+        MetaModel metaLeitura = MetaFactory.adicionarMetaValida();
+        metaLeitura.setMetaPaginasDia(-1);
+
+        MetaHelper.adicionarMeta(token, metaLeitura)
+                .then()
+                .log().body()
+                .statusCode(400)
+//                .body(equalTo("O ano informado não pode ser anterior ao corrente."))
+        ;
     }
 
 }
